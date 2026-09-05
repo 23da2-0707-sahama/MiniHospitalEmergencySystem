790c25c9a2ab51398a89c1db86d1cb4e20264f54
23da2-0707-sahama <23da2-0707.sltc.ac.lk>
2026-09-05 13:46:19 +0530

    Add visit search removal and history display

--- src/hospital/VisitHistory.java
+++ src/hospital/VisitHistory.java
@@ -1,6 +1,7 @@
 package hospital;
 
 
+
 public class VisitHistory {
 
     private class Node {
@@ -33,6 +34,71 @@ public class VisitHistory {
         }
 
         current.next = newNode;
+    }
+
+    public Visit searchVisit(int visitId) {
+
+        Node current = head;
+
+        while (current != null) {
+
+            if (current.visit.getVisitId() == visitId) {
+                return current.visit;
+            }
+
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    public void removeVisit(int visitId) {
+
+        if (head == null) {
+            return;
+        }
+
+        if (head.visit.getVisitId() == visitId) {
+
+            head = head.next;
+
+            return;
+        }
+
+        Node current = head;
+
+        while (current.next != null) {
+
+            if (current.next.visit.getVisitId() == visitId) {
+
+                current.next = current.next.next;
+
+                return;
+            }
+
+            current = current.next;
+        }
+    }
+
+    public void displayHistory() {
+
+        if (head == null) {
+
+            System.out.println(
+                    "No visit history."
+            );
+
+            return;
+        }
+
+        Node current = head;
+
+        while (current != null) {
+
+            System.out.println(current.visit);
+
+            current = current.next;
+        }
     }
 }
 
