0303754703bb627ba1975140f16e07bb92a631c6
23da2-0707-sahama <23da2-0707.sltc.ac.lk>
2026-09-05 13:31:15 +0530

    Create treatment record model

---
+++ src/hospital/TreatmentRecord.java
@@ -0,0 +1,39 @@
+package hospital;
+
+
+public class TreatmentRecord {
+
+    private int patientId;
+    private String patientName;
+    private String treatment;
+    private String doctorName;
+
+    public TreatmentRecord(int patientId,
+                           String patientName,
+                           String treatment,
+                           String doctorName) {
+
+        this.patientId = patientId;
+        this.patientName = patientName;
+        this.treatment = treatment;
+        this.doctorName = doctorName;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+}
+
+
